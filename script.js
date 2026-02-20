f84f9732ff94f9243ec0c9837eeecbc7e1a1dcae
JB-Aperture <jburnaby@apertureiot.com>
2026-02-21 07:27:38 +1000

    Update GAS URL

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
  */
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSVe8hHHlMGQDDF7v-Uq9Pocrhkpmfi9OeWlDiOwKGRO9A6JnyNnuP0AStsrGLAn7BMXwjmBMkRXArK/pub?gid=0&single=true&output=csv';
 const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbykXb0YeMoDNOaoIhpWwQIkeJnJCh8eJGn5LkhLsE6Ee-nkBFqdL1zKzbCbmTCzDYKT/exec';
-
 let brews = [];
 let sgChart = null;
 let currentBrewId = null;
